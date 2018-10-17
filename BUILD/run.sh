a66dc0a48efafb74d054152f0377b16f055b5542
Your Name <you@example.com>
2018-10-17 16:51:41 +0800

    dev_MAIN

--- BUILD/run.sh
+++ BUILD/run.sh
@@ -1,1 +1,5 @@
-
+cd ../STUDENT&&make
+cd ../LESSON&&make
+ar -rsv ../RELEASE/libmain.a ../RELEASE/stu.o ../RELEASE/less.o
+cd ../MAIN&&make
+cd ../RELEASE;./main
--- INCLUDE/pub.h
+++ INCLUDE/pub.h
@@ -1,1 +1,30 @@
+#ifndef _PUB_H
+#define _PUB_H
 
+int N1,N2;
+struct couse//课程信息结构体 
+{
+    int num1;
+    char name1[20];
+    char type[20];
+    int time;
+    int score;
+    int nelepeo;//课程已选人数
+    int Melepeo;//课程人数上限
+    struct couse * next;
+};
+
+struct student//学生信息结构体 
+{
+    int num2;
+    char name2[20];
+    int nelenum[50];//已选课程编号
+    int nelen;//已选课程总量
+    int nelescore; //总计学分
+    struct student *next;
+};
+
+struct couse * head1;
+struct student * head2;
+
+#endif
